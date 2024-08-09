f9db649c22fe1afba06df232de6e8e3d915f15ea
celestinaodili <odili.tina@gmail.com>
2024-08-09 07:15:33 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -8,7 +8,7 @@
         echo "Hello, this is my first web app!";        
     ?>
  <?php
-       echo "Hello, this is to test my first web app! CI/CD pipeline";
+       echo "This is to test my first web app! CI/CD pipeline";
     ?>
 </body>
 </html>

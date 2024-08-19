5619b154339becb3ddb2f0dc37696248a0ab8039
celestinaodili <odili.tina@gmail.com>
2024-08-19 15:37:29 +0100

    Updating content of  Index.php

--- Index.php
+++ Index.php
@@ -5,10 +5,15 @@
 </head>
 <body>
     <?php
-        echo "Hello, this is my first web app!";        
+        echo "Hello, this is my first web app! <br>";        
     ?>
  <?php
-       echo "This is to test my first web app! CI/CD pipeline";
+       echo "This is to test my first web app! CI/CD pipeline <br>";
+ echo "Continuous Integration (CI) is the practice of automatically building and 
+ testing code changes as they are integrated into a shared repository. <br>";
+ echo "Continuous Deployment (CD) extends this by automatically deploying code 
+ changes to production environments after successful tests. <br>";
+ 
     ?>
 </body>
 </html>
